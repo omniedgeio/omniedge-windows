b4f804197447aa72af00a9af5ce03e1cb8faca4f
mesut <137913279@qq.com>
2021-02-07 10:08:25 +0800

    1.remove autorun from code,which we can realize this function by setup

--- main.cpp
+++ main.cpp
@@ -13,10 +13,6 @@ int main(int argc, char *argv[])
     RunGuard guard( "Omniedge_Desktop_Client" );
     if ( !guard.tryToRun() )
         return 0;
-    //Startup item
-    QSettings  reg("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run",QSettings::NativeFormat);
-    QString strAppPath=QDir::toNativeSeparators(QCoreApplication::applicationFilePath());
-    reg.setValue("OmniEdge",strAppPath);
 
 #ifdef QT_NO_DEBUG
     syslog hlog;
