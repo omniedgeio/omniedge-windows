83c8ffff4a0dcaf81a7dc1a0a2ee0998afe25801
mesut <137913279@qq.com>
2021-02-23 14:42:04 +0800

    1.add contact & email in about dialog

--- aboutdialog.cpp
+++ aboutdialog.cpp
@@ -5,11 +5,18 @@ AboutDialog::AboutDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Dialog)
 {
+    //ui->label_contact->setText(QString::fromLocal8Bit("<a style='color: green;' href = www.doshow.com> 查看</a>"));
     ui->setupUi(this);
     this->setWindowFlags(Qt::CustomizeWindowHint | Qt::WindowTitleHint);
     adjustSize();
     setFixedSize(size());
     this->setModal(false);
+    ui->label_contact->setTextFormat(Qt::RichText);
+    ui->label_contact->setText(QString("Contact Us: ")+"<style> a {text-decoration: none} </style> <a href='mailto:support@omniedge.io'>support@omniedge.io</a>");
+    ui->label_contact->setOpenExternalLinks(true);
+    ui->label_twitter->setOpenExternalLinks(true);
+    ui->label_twitter->setText(QString("Twitter:")+QString::fromLocal8Bit("<style> a {text-decoration: none} </style> <a href = https://twitter.com/omniedgeio> @omniedgeio</a>"));
+
 }
 
 AboutDialog::~AboutDialog()
