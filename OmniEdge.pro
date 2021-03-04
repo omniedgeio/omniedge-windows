eeaefc9c2c0f14014b3012376deabd7e79126b55
Tex-Tang <tex_tang@outlook.com>
2021-03-05 00:00:13 +0800

    Add autoupdate feature

--- OmniEdge.pro
+++ OmniEdge.pro
@@ -1,10 +1,10 @@
-QT       += core gui network networkauth
+QT       += core gui network networkauth autoupdatercore
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
 QMAKE_LFLAGS_WINDOWS += /MANIFESTUAC:"level='requireAdministrator'"
-
-CONFIG += c++11
+QMAKE_CXXFLAGS += /std:c++17 /std:latest
+CONFIG += c++17
 
 # The following define makes your compiler emit warnings if you use
 # any Qt feature that has been marked deprecated (the exact warnings
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "traymenu.h"
 #include <QApplication>
 #include <QSettings>
+#include <QtAutoUpdaterCore/Updater>
 #include "menucontroller.h"
 #include "syslog.h"
 #include "runguard.h"
@@ -25,6 +26,25 @@ int main(int argc, char *argv[])
     QCoreApplication::setApplicationName("Omniedge Desktop Client");
 
     TrayMenu menu;
+
+    //create the updater with the application as parent -> will live long enough start the tool on exit
+     /*   auto updater = new QtAutoUpdater::Updater::create("qtifw", {
+            {"path", "C:/Program Files (x86)/OmniEdge/MaintenanceTool"} //.exe or .app is automatically added on the platform
+        }, &a);
+
+        QObject::connect(updater, &QtAutoUpdater::Updater::checkUpdatesDone, [updater](QtAutoUpdater::Updater::State state) {
+            qDebug() << "Update result:" << state;
+            if (state == QtAutoUpdater::Updater::State::NewUpdates) {
+                //As soon as the application quits, the maintenancetool will be started in update mode
+                qDebug() << "Update info:" << updater->updateInfo();
+                updater->runUpdater();
+            }
+            //Quit the application
+            qApp->quit();
+        });
+
+        //start the update check
+        updater->checkForUpdates();*/
 
     return a.exec();
 }
